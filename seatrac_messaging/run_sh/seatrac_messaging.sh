bcdd31efafd93f2ebd78cf538ab2bd7c1dc1bb4e
isabelleviraldo <isabelleviraldo@gmail.com>
2025-08-07 11:45:19 -0700

    fix sh run file

--- seatrac_messaging/run_sh/seatrac_messaging.sh
+++ seatrac_messaging/run_sh/seatrac_messaging.sh
@@ -7,20 +7,11 @@ set -euo pipefail
 read -rp "Local SeaTrac ID? " self_beacon_id
 read -rp "Remote SeaTrac ID? " destination_id
 
-#setup so that it is viewable from wherever
-export ROS_DOMAIN_ID=0
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI="<CycloneDDS><Domain><General><AllowMulticast>false</AllowMulticast><Interfaces><NetworkInterface>lo</NetworkInterface></Interfaces></General></Domain></CycloneDDS>"
-
 # Source ROS 2 + workspace setup files
 set +u  # allow unset variables during sourcing
 source /opt/ros/foxy/setup.bash
 source /ros2_ws/install/setup.bash
 set -u
-
-#for some reason ros topics arent exposed unless we do this
-ros2 daemon stop
-ros2 daemon start
 
 # Launch the ROS 2 launch file with parameters
 exec ros2 launch seatrac_messaging seatrac_messaging.launch.py \
